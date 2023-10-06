eaa673dffc19854397106dd816271af5e788194a
Tristan Rechenberger <rechenberger@me.com>
2023-10-06 09:51:02 +0200

    🎉

--- src/components/3d/Galaxy.tsx
+++ src/components/3d/Galaxy.tsx
@@ -1,11 +1,11 @@
-import { Grid, Stars } from '@react-three/drei'
+import { Stats } from '@react-three/drei'
 import { Canvas } from '@react-three/fiber'
 import { Physics } from '@react-three/rapier'
 import { Asteroids } from './Asteroids'
 import { Camera } from './Camera'
+import { GalaxyEnvironment } from './GalaxyEnvironment'
 import { Player } from './Player'
 import { Systems } from './Systems'
-import { GalaxyEnvironment } from './GalaxyEnvironment'
 
 const DEBUG = false
 
@@ -13,6 +13,7 @@ export const Galaxy = () => {
   return (
     <>
       <Canvas className="bg-gradient-to-tr from-black to-black bg-blend-darken flex-1">
+        <Stats />
         <Physics
           gravity={[0, 0, 0]}
           colliders={false}
