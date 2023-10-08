d17922c2f571c626d44190dd5f50aff25a8afcf7
Tristan Rechenberger <rechenberger@me.com>
2023-10-08 17:18:52 +0200

    🎉

--- src/app/galaxy/page.tsx
+++ src/app/galaxy/page.tsx
@@ -1,20 +1,5 @@
-'use client'
-
-import dynamic from 'next/dynamic'
-import { Suspense } from 'react'
-
-const Galaxy = dynamic(() => import('@/components/3d/Galaxy'), {
-  ssr: false,
-})
+import { redirect } from 'next/navigation'
 
 export default function Page() {
-  return (
-    <>
-      <div className="relative h-[100svh]">
-        <Suspense>
-          <Galaxy />
-        </Suspense>
-      </div>
-    </>
-  )
+  return redirect('/')
 }
