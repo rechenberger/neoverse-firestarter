5a14899081a724fc2614fdf2f202b91c3ed6368f
Tristan Rechenberger <rechenberger@me.com>
2023-10-08 21:13:49 +0200

    🎉

--- src/components/3d/UpgradeMenu.tsx
+++ src/components/3d/UpgradeMenu.tsx
@@ -1,11 +1,8 @@
+import { Heart, Shield } from 'lucide-react'
 import { proxy, useSnapshot } from 'valtio'
-import {
-  Sheet,
-  SheetContent,
-  SheetDescription,
-  SheetHeader,
-  SheetTitle,
-} from '../ui/sheet'
+import { Button } from '../ui/button'
+import { Card } from '../ui/card'
+import { Sheet, SheetContent, SheetTitle } from '../ui/sheet'
 
 export const upgradeMenuState = proxy({
   open: false,
@@ -20,18 +17,76 @@ export const UpgradeMenu = () => {
         onOpenChange={(open) => (upgradeMenuState.open = open)}
       >
         <SheetContent
-          className="w-[400px] sm:w-[540px] mx-auto rounded-t-xl"
+          className="w-[400px] sm:w-[540px] mx-auto rounded-t-xl flex flex-col gap-2"
           side={'bottom'}
         >
-          <SheetHeader>
-            <SheetTitle>Upgrades</SheetTitle>
-            <SheetDescription>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem
-              quam fugiat ipsa alias veniam a fugit esse deleniti ullam,
-              dolorum, dolor blanditiis, natus eos hic quas officia facere et
-              distinctio?
-            </SheetDescription>
-          </SheetHeader>
+          <SheetTitle>Resources</SheetTitle>
+          <div className="grid gap-2 grid-cols-2 sm:grid-cols-3">
+            <Card className="flex flex-row gap-2 px-2 py-1 items-center text-sm">
+              <div className="h-3 w-3 rounded-full bg-neutral-500" />
+              <div>
+                <strong>500</strong>
+                <span>&nbsp;Iron</span>
+              </div>
+            </Card>
+            <Card className="flex flex-row gap-2 px-2 py-1 items-center text-sm">
+              <div className="h-3 w-3 rounded-full bg-yellow-500" />
+              <div>
+                <strong>120</strong>
+                <span>&nbsp;Silicone</span>
+              </div>
+            </Card>
+            <Card className="flex flex-row gap-2 px-2 py-1 items-center text-sm">
+              <div className="h-3 w-3 rounded-full bg-red-500" />
+              <div>
+                <strong>34</strong>
+                <span>&nbsp;Aluminum</span>
+              </div>
+            </Card>
+          </div>
+          <div className="h-2" />
+          <SheetTitle>Upgrades</SheetTitle>
+          <div className="grid gap-2 grid-cols-2">
+            <Card className="flex flex-col items-center gap-0 p-2">
+              <div className="font-bold">Health</div>
+              <div className="text-sm opacity-60">Level 4/10</div>
+              <Heart className="w-8 h-8 my-4" />
+              <Button
+                variant={'secondary'}
+                disabled
+                className="p-2 flex flex-col gap-1 h-auto items-stretch"
+              >
+                <Card className="flex flex-row gap-2 px-2 py-1 items-center text-sm">
+                  <div className="h-3 w-3 rounded-full bg-neutral-500" />
+                  <div>
+                    <strong>600</strong>
+                    <span>&nbsp;Iron</span>
+                  </div>
+                </Card>
+              </Button>
+            </Card>
+            <Card className="flex flex-col items-center gap-0 p-2">
+              <div className="font-bold">Shield</div>
+              <div className="text-sm opacity-60">Level 0/10</div>
+              <Shield className="w-8 h-8 my-4" />
+              <Button className="p-2 flex flex-col gap-1 h-auto items-stretch">
+                <Card className="flex flex-row gap-2 px-2 py-1 items-center text-sm">
+                  <div className="h-3 w-3 rounded-full bg-yellow-500" />
+                  <div>
+                    <strong>20</strong>
+                    <span>&nbsp;Silicone</span>
+                  </div>
+                </Card>
+                <Card className="flex flex-row gap-2 px-2 py-1 items-center text-sm">
+                  <div className="h-3 w-3 rounded-full bg-red-500" />
+                  <div>
+                    <strong>10</strong>
+                    <span>&nbsp;Aluminum</span>
+                  </div>
+                </Card>
+              </Button>
+            </Card>
+          </div>
         </SheetContent>
       </Sheet>
     </>
