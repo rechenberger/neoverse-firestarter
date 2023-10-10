84b79a3705bd042b0c2efee14927e19c3a9e6bab
Tristan Rechenberger <rechenberger@me.com>
2023-10-10 02:31:25 +0200

    🎉

--- src/components/3d/HudTopRight.tsx
+++ src/components/3d/HudTopRight.tsx
@@ -1,13 +1,32 @@
+import { Github } from 'lucide-react'
+import Link from 'next/link'
 import { DarkModeToggle } from '../layout/DarkModeToggle'
+import { Button } from '../ui/button'
 import { MusicButton } from './MusicButton'
 
 export const HudTopRight = () => {
   return (
     <>
       <div className="absolute top-4 right-4 flex flex-row gap-2">
+        <GithubButton />
         <MusicButton />
         <DarkModeToggle />
       </div>
+    </>
+  )
+}
+
+const GithubButton = () => {
+  return (
+    <>
+      <Link
+        href="https://github.com/rechenberger/neoverse-firestarter"
+        target="_blank"
+      >
+        <Button variant="outline" size="icon">
+          <Github className="w-4 h-4" />
+        </Button>
+      </Link>
     </>
   )
 }
